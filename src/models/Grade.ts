97b2d7e039412f9572ffa1fae137ea5aa29ad1d9
Kin-Doung <kin.doung@student.passerellesnumeriques.org>
2025-06-02 08:52:38 +0700

    create grade #10

--- src/models/Grade.ts
+++ src/models/Grade.ts
@@ -1,3 +1,13 @@
-export class Grade{
-    
+import { Assignment } from './Assignment';
+
+export class Grade {
+    assignment: Assignment;
+    score: number;
+    isMissing: boolean;
+
+    constructor(assignment: Assignment, score: number, isMissing: boolean) {
+        this.assignment = assignment;
+        this.score = score;
+        this.isMissing = isMissing;
+    }
 }
